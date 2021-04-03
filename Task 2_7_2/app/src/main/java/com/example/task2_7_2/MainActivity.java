e01e1b120e322800fe7ef2f3a7f01c287ad55a0d
KomogortsevaYulia <74527737+KomogortsevaYulia@users.noreply.github.com>
2021-04-03 19:45:20 +0800

    2_7_2

---
+++ Task 2_7_2/app/src/main/java/com/example/task2_7_2/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.task2_7_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    EditText Login;
+    EditText Password;
+    Button Input;
+    List<String[]> list=new ArrayList<>();
+    boolean r;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list.add(new String[]{"Yulia", "123"});
+        list.add(new String[]{"123", "123"});
+        text=(TextView) findViewById(R.id.textView);
+        Login=(EditText) findViewById(R.id.editTextLogin);
+        Password=(EditText)findViewById(R.id.editTextPassword);
+        Input=(Button)findViewById(R.id.buttonInput);
+        text.setText("");
+        Input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (String[] a:list) {
+                    if (String.valueOf(Login.getText()).equals(a[0]) & String.valueOf(Password.getText()).equals(a[1]) ){
+                        text.setText(R.string.True);
+                        text.setTextColor(Color.GREEN);
+                        r=true;
+                        return;
+                    }else {
+                        r=false;
+                    }
+                }
+                if (!r){
+                    Context context=MainActivity.this;
+                    Class anyActivity=MainActivity2.class;
+                    Intent anyActiviy=new Intent(context,anyActivity);
+                    startActivityForResult(anyActiviy,1);
+                }
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle arguments = getIntent().getExtras();
+        switch (resultCode) {
+            case RESULT_OK:
+                Login.setText(data.getStringExtra("loginValue"));
+                String log = data.getStringExtra("Log");
+                String pas = data.getStringExtra("Pas");
+                list.add(new String[]{log, pas});
+        }
+    }
+}
---
+++ Task 2_7_2/app/src/main/java/com/example/task2_7_2/MainActivity2.java
@@ -0,0 +1,42 @@
+package com.example.task2_7_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+    EditText Login;
+    EditText Password;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Login=(EditText) findViewById(R.id.editLogin);
+        Password=(EditText)findViewById(R.id.editPassword);
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!Login.getText().toString().equals("") && !Password.getText().toString().equals("")) {
+                    Intent i = new Intent();
+                    i.putExtra("loginValue", Login.getText().toString());
+                    i.putExtra("passwordValue", Password.getText().toString());
+                    setResult(RESULT_OK, i);
+                    finish();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Вы ничего не ввели", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
